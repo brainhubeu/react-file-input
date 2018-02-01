b579d1d258e4e2970c53d9561d5a5237b3628bef
Alejandro Hernandez <alejandro@brainhub.pl>
2018-02-01 15:47:15 +0100

    Boilerplate setup

---
+++ example/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import FileInput from '../../src';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React File Input</h1>
+        <h2>By Brainhub</h2>
+        <FileInput/>
+      </div>
+    );
+  }
+}
---
+++ src/components/FileInput.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class FileInput extends Component {
+  render() {
+    return (<input type="file" />);
+  }
+}
