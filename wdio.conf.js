3edb79d34e5aca48225d8c39f0487370a5f2626d
Alejandro Hernandez <alejandro@brainhub.pl>
2018-02-02 10:58:39 +0100

    Remove unnecessary stuff

--- wdio.conf.js
+++ wdio.conf.js
@@ -87,14 +87,14 @@ exports.config = {
   // WebdriverRTC: https://github.com/webdriverio/webdriverrtc
   // Browserevent: https://github.com/webdriverio/browserevent
   // plugins: {
-    //     webdrivercss: {
-      //         screenshotRoot: 'my-shots',
-      //         failedComparisonsRoot: 'diffs',
-      //         misMatchTolerance: 0.05,
-      //         screenWidth: [320,480,640,1024]
-    //     },
-    //     webdriverrtc: {},
-    //     browserevent: {}
+  //     webdrivercss: {
+  //         screenshotRoot: 'my-shots',
+  //         failedComparisonsRoot: 'diffs',
+  //         misMatchTolerance: 0.05,
+  //         screenWidth: [320,480,640,1024]
+  //     },
+  //     webdriverrtc: {},
+  //     browserevent: {}
   // },
   //
   // Test runner services
@@ -118,19 +118,19 @@ exports.config = {
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {
-    require: ['./features/step_definitions'],        // <string[]> (file/dir) require files before executing features
-    backtrace: false,   // <boolean> show full backtrace for errors
-    compiler: [],       // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-    dryRun: false,      // <boolean> invoke formatters without executing steps
-    failFast: false,    // <boolean> abort the run on first failure
+    require: ['./features/step_definitions'], // <string[]> (file/dir) require files before executing features
+    backtrace: false, // <boolean> show full backtrace for errors
+    compiler: [], // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    dryRun: false, // <boolean> invoke formatters without executing steps
+    failFast: false, // <boolean> abort the run on first failure
     format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
-    colors: true,       // <boolean> disable colors in formatter output
-    snippets: true,     // <boolean> hide step definition snippets for pending steps
-    source: true,       // <boolean> hide source uris
-    profile: [],        // <string[]> (name) specify the profile to use
-    strict: false,      // <boolean> fail if there are any undefined or pending steps
-    tags: ['~@skip'],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
-    timeout: 20000,     // <number> timeout for step definitions
+    colors: true, // <boolean> disable colors in formatter output
+    snippets: true, // <boolean> hide step definition snippets for pending steps
+    source: true, // <boolean> hide source uris
+    profile: [], // <string[]> (name) specify the profile to use
+    strict: false, // <boolean> fail if there are any undefined or pending steps
+    tags: ['~@skip'], // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    timeout: 20000, // <number> timeout for step definitions
     ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
   },
   //
@@ -196,5 +196,5 @@ exports.config = {
   // Gets executed after all workers got shut down and the process is about to exit. It is not
   // possible to defer the end of the process using a promise.
   // onComplete: function(exitCode) {
-    // }
+  // }
 };
