f3f6b8fba7d65370b5d001cdf41558ade2685c87
Mateusz Drażyk <=mateusz.drazyk@brainhub.pl>
2018-02-06 14:38:37 +0100

    Added styles to metadata component

--- src/components/FileInputMetadata.js
+++ src/components/FileInputMetadata.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import filesize from 'filesize';
 import { string, number } from 'prop-types';
 import '../styles/FileInputMetadata.scss';
 
-const FileInputMetadata = props => <div className="brainhubFileInput__metadata">
-  <div className="brainhubFileInput__metadata__name">{props.name}</div> ({props.size})
-</div>;
+const styleName = name => {
+  // Do we have list of supported image types ??
+  const imageTypes = ['.jpg', '.jpeg', '.png', '.gif'];
+  const typeIndex = imageTypes.map(type => name.indexOf(type));
+
+  return {
+    name: name.slice(0, typeIndex[0]),
+    type: name.slice(typeIndex[0]),
+  };
+};
+
+const FileInputMetadata = props => {
+  const { name, type } = styleName(props.name);
+  return (<div className="brainhubFileInput__metadata">
+    <div className="brainhubFileInput__metadata__image">
+
+      {/* This is placeholder for now */}
+      <div style={{ width: '20px', height: '20px', backgroundColor: '#5680BB' }}></div>
+
+    </div>
+    <div className="brainhubFileInput__metadata__info">
+      <span className="brainhubFileInput__metadata__info_name">{name}</span>{type} ({filesize(props.size, { separator: ',' })})</div>
+  </div>);
+};
 
 FileInputMetadata.propTypes = {
   name: string.isRequired,
