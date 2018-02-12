fddb5c499028e6f939de01d03da6b592dd51246e
Alejandro Hernandez <alejandro@brainhub.pl>
2018-02-12 22:35:57 +0100

    resizeCorner

--- test/helpers/imageEdition.spec.js
+++ test/helpers/imageEdition.spec.js
@@ -1,6 +1,4 @@
-import { move, resizeSide } from '../../src/helpers/imageEdition';
-
-// ({ pointX, pointY }, { height, width }, { xM, yM }, { x0, y0, x1, y1 })
+import { move, resizeCorner, resizeSide } from '../../src/helpers/imageEdition';
 
 describe('helpers', () => {
   describe('move', () => {
@@ -81,8 +79,146 @@ describe('helpers', () => {
       });
     });
   });
+  describe('resizeCorner', () => {
+    describe('without ratio', () => {
+      it('should return the right point', () => {
+        const click = { pointX: 0, pointY: 90 };
+        const canvas = { height: 100, width: 200 };
+        const point = { x0: 50, y0: 25, x1: 150, y1: 75 };
+        const values = resizeCorner(click, canvas, point, true);
+
+        expect(values).toEqual({ x1: click.pointX, y1: click.pointY });
+      });
+    });
+    describe('vertical movement with ratio', () => {
+      it('should return a valid area', () => {
+        const click = { pointX: 0, pointY: 80 };
+        const canvas = { height: 100, width: 200 };
+        const point = { x0: 50, y0: 20, x1: 60, y1: 50 };
+        const ratio = 1/3;
+        const values = resizeCorner(click, canvas, point, true, ratio);
+
+        expect(values).toEqual({
+          x1: point.x1 + ((click.pointY - point.y1) * ratio),
+          y1: click.pointY,
+        });
+      });
+      it('should return a valid area if new point is outside the canvas', () => {
+        const click = { pointX: 0, pointY: 120 };
+        const canvas = { height: 100, width: 200 };
+        const point = { x0: 50, y0: 20, x1: 60, y1: 50 };
+        const ratio = 1/3;
+        const values = resizeCorner(click, canvas, point, true, ratio);
+
+        expect(values).toEqual({
+          x1: point.x1 + ((canvas.height - point.y1) * ratio),
+          y1: canvas.height,
+        });
+      });
+      it('should return a valid area if the resize makes it change the sign and it is outside the canvas', () => {
+        const click = { pointX: 0, pointY: 0 };
+        const canvas = { height: 100, width: 200 };
+        const point = { x0: 50, y0: 20, x1: 60, y1: 50 };
+        const ratio = 1/3;
+        const values = resizeCorner(click, canvas, point, true, ratio);
+
+        expect(values).toEqual({
+          x1: point.x1 + ((0 - point.y1) * ratio),
+          y1: 0,
+        });
+      });
+      it('should return a valid area if points are inverted', () => {
+        const click = { pointX: 0, pointY: 10 };
+        const canvas = { height: 100, width: 200 };
+        const point = { x0: 50, y0: 70, x1: 60, y1: 40 };
+        const ratio = 1/3;
+        const values = resizeCorner(click, canvas, point, true, ratio);
+
+        expect(values).toEqual({
+          x1: point.x1 - ((click.pointY - point.y1) * ratio),
+          y1: click.pointY,
+        });
+      });
+      it('should respect the horizontal dimensions', () => {
+        const click = { pointX: 0, pointY: 90 };
+        const canvas = { height: 100, width: 200 };
+        const point = { x0: 185, y0: 0, x1: 195, y1: 30 };
+        const ratio = 1/3;
+        const values = resizeCorner(click, canvas, point, true, ratio);
+
+        expect(values).toEqual({
+          x1: canvas.width,
+          y1: ((canvas.width - point.x1) / ratio) + point.y1,
+        });
+      });
+    });
+    describe('horizontal movement with ratio', () => {
+      it('should return a valid area', () => {
+        const click = { pointX: 80, pointY: 0 };
+        const canvas = { height: 200, width: 100 };
+        const point = { x0: 20, y0: 50, x1: 50, y1: 60 };
+        const ratio = 3;
+        const values = resizeCorner(click, canvas, point, false, ratio);
+
+        expect(values).toEqual({
+          x1: click.pointX,
+          y1: point.y1 + ((click.pointX - point.x1) / ratio),
+        });
+      });
+      it('should return a valid area if new point is outside the canvas', () => {
+        const click = { pointX: 120, pointY: 0 };
+        const canvas = { height: 200, width: 100 };
+        const point = { x0: 20, y0: 50, x1: 50, y1: 60 };
+        const ratio = 3;
+        const values = resizeCorner(click, canvas, point, false, ratio);
+
+        expect(values).toEqual({
+          x1: canvas.width,
+          y1: point.y1 + ((canvas.width - point.x1) / ratio),
+        });
+      });
+      it('should return a valid area if the resize makes it change the sign and it is outside the canvas', () => {
+        const click = { pointX: 0, pointY: 0 };
+        const canvas = { height: 200, width: 100 };
+        const point = { x0: 20, y0: 50, x1: 50, y1: 60 };
+        const ratio = 3;
+        const values = resizeCorner(click, canvas, point, false, ratio);
+
+        values.y1 = Math.floor(values.y1 * 100) / 100;
+
+        expect(values).toEqual({
+          x1: 0,
+          y1: point.y1 + (Math.floor(((0 - point.x1) / ratio) * 100) / 100),
+        });
+      });
+      it('should return a valid area if points are inverted', () => {
+        const click = { pointX: 10, pointY: 0 };
+        const canvas = { height: 200, width: 100 };
+        const point = { x0: 70, y0: 50, x1: 40, y1: 60 };
+        const ratio = 3;
+        const values = resizeCorner(click, canvas, point, false, ratio);
+
+        expect(values).toEqual({
+          x1: click.pointX,
+          y1: point.y1 - ((click.pointX - point.x1) / ratio),
+        });
+      });
+      it('should respect the horizontal dimensions', () => {
+        const click = { pointX: 90, pointY: 0 };
+        const canvas = { height: 200, width: 100 };
+        const point = { x0: 0, y0: 185, x1: 30, y1: 195 };
+        const ratio = 3;
+        const values = resizeCorner(click, canvas, point, false, ratio);
+
+        expect(values).toEqual({
+          x1: ((canvas.height - point.y1) * ratio) + point.x1,
+          y1: canvas.height,
+        });
+      });
+    });
+  });
   describe('resizeSide', () => {
-    describe('vertical movement without ratio', () => {
+    describe('vertical  without ratio', () => {
       it('should return a valid area', () => {
         const click = { pointX: 0, pointY: 90 };
         const canvas = { height: 100, width: 200 };
