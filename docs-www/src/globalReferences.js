700005bd44bf1e47d687f577b26a2691c81273ae
roberthebel <robert@brainhub.pl>
2020-05-18 10:04:47 +0200

    PR fixes

--- docs-www/src/globalReferences.js
+++ docs-www/src/globalReferences.js
@@ -4,13 +4,13 @@ const { default: FileInput } = (() => {
   if (!global.window) {
     global.window = {};
   }
-  // if ('__RC_ENV__' === 'development') {
-    console.log('connecting with local react-carousel source code');
+  if ('__RC_ENV__' === 'development') {
+    console.log('connecting with local react-file-input source code');
     return require('../../src');
-  // } else {
-  //   console.log('connecting with @brainhubeu/react-carousel installed in node_modules');
-  //   return require('@brainhubeu/react-file-input');
-  // }
+  } else {
+    console.log('connecting with @brainhubeu/react-file-input installed in node_modules');
+    return require('@brainhubeu/react-file-input');
+  }
 })();
 
 
